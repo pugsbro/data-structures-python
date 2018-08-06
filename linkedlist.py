03c670ce1098ce534506c80260390b733bbe321b
Bradley Scott <bradleyscott@Bradleys-MacBook-Pro.local>
2018-08-07 00:06:33 +0100

    Initial commit

---
+++ linkedlist.py
@@ -0,0 +1,100 @@
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval 
+        self.next = None 
+
+class LinkedList:
+    def __init__(self):
+        self.headval = None
+
+    def printlist(self):
+        printnode = self.headval
+        while (printnode!=None):
+            print(printnode.dataval)
+            printnode = printnode.next
+
+    def append(self, newdata):
+        #add to end of list 
+        newnode = Node(newdata)
+
+        if self.headval == None:
+            self.headval = newnode
+            return
+        
+        last = self.headval
+        while (last.next):
+            last = last.next
+
+        last.next = newnode
+
+    def start(self, newdata):
+        #add to beginning of list 
+        newnode = Node(newdata)
+        newnode.next = self.headval
+        self.headval = newnode
+
+    def after(self, node, newdata):
+        newnode = Node(newdata)
+        newnode.next = node.next
+        node.next = newnode
+        
+    def remove(self,removekey):
+        Head = self.headval
+
+        if Head != None:
+            if Head.dataval == removekey:
+                self.headval = Head.next 
+                return 
+        
+        while Head!=None:
+            if Head.next.dataval == removekey:
+                break
+            Head = Head.next
+        
+        Head.next = Head.next.next 
+       
+
+    def size(self):
+
+        count = 0
+        countNode = self.headval        
+        while(countNode!=None):
+            countNode = countNode.next
+            count = count + 1
+
+        print count 
+
+
+
+
+
+list1 = LinkedList()
+list1.headval = Node("pugs")
+
+entry2 = Node("frenchie")
+list1.headval.next = entry2
+
+entry3 = Node("lab")
+entry3.next = entry3
+
+list1.printlist()
+
+list1.append("Doug")
+
+list1.printlist()
+
+list1.start("lou lou the pug ")
+
+list1.printlist()
+
+list1.after(list1.headval, "hamlet")
+
+list1.printlist()       
+
+list1.size() 
+
+list1.remove("hamlet")
+list1.remove("frenchie")
+
+
+list1.printlist()
